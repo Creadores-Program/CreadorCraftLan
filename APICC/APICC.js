61c4b1dba940cea646c16fe523673cb5419b5c72
Trollhunters501PC <cpcreador@gmail.com>
2025-05-03 12:49:42 -0600

    Añadir API JS y Linux Fedora

---
+++ APICC/APICC.js
@@ -0,0 +1,27 @@
+class APICreadoCraftLan{
+    static urlPrefix = "OpenCreadorCraftLan://";
+    carpeta = "";
+    jsonToServer = [];
+    constructor(carpeta){
+        this.carpeta = carpeta;
+    }
+    async start(){
+        await GameProps.getFileGame().folder(this.carpeta).forEach(async function(relativePath, file){
+            if(file.dir){
+                return;
+            }
+            if(relativePath.endsWith(".json") || relativePath.endsWith(".js") || relativePath.endsWith(".txt") || relativePath.endsWith(".xml") || relativePath.endsWith(".html") || relativePath.endsWith(".css") || relativePath.endsWith(".md") || relativePath.endsWith(".properties") || relativePath.endsWith(".yml") || relativePath.endsWith(".yaml") || relativePath.endsWith(".log")){
+                this.jsonToServer.push({
+                    path: relativePath,
+                    content: await file.async("string")
+                });
+            }else{
+                this.jsonToServer.push({
+                    path: relativePath,
+                    content: await file.async("base64")
+                });
+            }
+        }.bind(this));
+        window.open(this.urlPrefix + btoa(JSON.stringify(this.jsonToServer)), "_blank");
+    }
+}
